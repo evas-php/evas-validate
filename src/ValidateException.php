a8d33ed857294caab65a766d479a8049bfca320d
Egor Vasyakin <evasyakin@mail.ru>
2020-01-09 14:01:29 +0700

    Добавлен класс исключения валидации

---
+++ src/ValidateException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package evas-php/evas-validate
+ */
+namespace Evas\Validate;
+
+use Evas\Base\Exception;
+
+/**
+ * Класс исключений валидации.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class ValidateException extends Exception
+{
+}
