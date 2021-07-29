ce02db68c91d9c1dc4e4c4c60703cfd78e119bbd
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-29 15:36:40 +0300

    Create Fieldset.php

---
+++ src/Fieldset.php
@@ -0,0 +1,259 @@
+<?php
+/**
+ * Валидатор набора полей.
+ * @package evas-php\evas-validate
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Validate;
+
+use \InvalidArgumentException;
+use Evas\Base\Help\PhpHelp;
+use Evas\Validate\Errors;
+use Evas\Validate\HtmlEscapingTrait;
+use Evas\Validate\Field;
+use Evas\Validate\Interfaces\ValidatableInterface;
+use Evas\Validate\ValidateErrorBuilder;
+use Evas\Validate\ValidateException;
+
+if (!defined('EVAS_VALIDATE_DEFAULT_FIELD_CLASS')) {
+    define('EVAS_VALIDATE_DEFAULT_FIELD_CLASS', Field::class);
+}
+
+class Fieldset implements ValidatableInterface
+{
+    /**
+     * Подключаем поддержкуэкранирования html-тегов.
+     */
+    use HtmlEscapingTrait;
+
+    /** @var string имя набора полей, если это вложенный набор полей */
+    public $name;
+    /** @var array валидаторы полей [имя поля => валидатор] */
+    public $fields = [];
+    /** @var Errors объект ошибок */
+    protected $errors;
+    /** @var array значения после валидации [имя поля => значение] */
+    public $values = [];    
+    /** @var array исходные значения до валидации [имя поля => значение] */
+    public $valuesBefore = [];
+
+    /** @var string класс валидатора поля по умолчанию */
+    public $defaultFieldClass = EVAS_VALIDATE_DEFAULT_FIELD_CLASS;
+
+    public $valuesTypeError;
+
+    /**
+     * Предустановленные поля валидатора.
+     * @return array|null
+     */
+    public function presetFields(): ?array
+    {
+        return null;
+    }
+
+    /**
+     * Конструктор.
+     * @param array|null [имя поля => валидатор] или [имя поля => параметры валидатора]
+     * @param array|null параметры валидатора набора полей
+     */
+    public function __construct(array $fields = null, array $props = null)
+    {
+        $fields = array_merge($this->presetFields() ?? [], $fields ?? []);
+        if ($fields) $this->fields($fields);
+        if (!empty($props)) foreach ($props as $name => $value) {
+            $this->$name = $value;
+        }
+    }
+
+    /**
+     * Установка класса валидатора поля по умолчанию.
+     * @param string имя класса поля по умолчанию
+     * @return self
+     * @throws InvalidArgumentException
+     */
+    public function defaultFieldClass(string $defaultFieldClass)
+    {
+        if (!is_subclass_of($defaultFieldClass, Field::class)) {
+            throw new InvalidArgumentException(sprintf(
+                'Argument 1 passed to %s() must be an instance or a child of the %s, %s given', 
+                __METHOD__, Field::class, PhpHelp::getType($field)
+            ));
+        }
+        $this->defaultFieldClass = $defaultFieldClass;
+        return $this;
+    }
+
+    /**
+     * Установка валидаторов полей.
+     * @param array маппинг валидаторов или параметров валидаторов по именам полей
+     * @return self
+     */
+    public function fields(array $fields)
+    {
+        foreach ($fields as $name => $field) {
+            $this->field($name, $field);
+        }
+        return $this;
+    }
+
+    /**
+     * Установка валидатора поля.
+     * @param string имя поля
+     * @param array|Field валидатор или параметры валидатора
+     * @return self
+     * @throws InvalidArgumentException
+     */
+    public function field(string $name, $field)
+    {
+        if (is_array($field)) {
+            $field = new $this->defaultFieldClass($field);
+        }
+        if (!($field instanceof Field || $field instanceof self)) {
+            throw new InvalidArgumentException(sprintf(
+                'Argument 2 passed to %s() must be an array or an instance or a child of the %s or %s, %s given', 
+                __METHOD__, Field::class, Fieldset::class, 
+                PhpHelp::getType($field)
+            ));
+        }
+        $field->name = $name;
+        $this->fields[$name] = $field;
+        return $this;
+    }
+
+    /**
+     * Получение вложенного поля или набора полей.
+     * @return ValidatableInterface|null вложенное поле или набор полей
+     */
+    public function getField(string $name): ?ValidatableInterface
+    {
+        return $this->fields[$name] ?? null;
+    }
+
+    /**
+     * Получение объекта ошибок.
+     * @return Errors
+     */
+    public function errors()
+    {
+        if (null === $this->errors) $this->errors = new Errors;
+        return $this->errors;
+    }
+
+    /**
+     * Установка ошибки/ошибок в список.
+     * @param string|array ключ поля или маппинг ошибок по полям
+     * @param string|null ошибка, если не маппинг
+     * @throws InvalidArgumentException
+     */
+    public function setError($key, string $value = null)
+    {
+        if (!is_string($key) && !is_array($key)) {
+            throw new InvalidArgumentException(sprintf('Argument 1 $key must be a string or an array, %s given', gettype($key)));
+        }
+        if (empty($this->name)) {
+            $this->errors()->set($key, $value);
+        } else {
+            // если вложенный набор полей
+            if (is_string($key)) $key = [$key => $value];
+            foreach ($key as $subkey => $subvalue) {
+                $subkey = "$this->name.$subkey";
+                $this->errors()->set($subkey, $subvalue);
+            }
+        }
+    }
+
+    /**
+     * Сборка ошибки вложенного валидатора полей.
+     * @param string тип ошибки
+     * @return false
+     */
+    public function buildError(string $errorType)
+    {
+        $message = ValidateErrorBuilder::build($errorType, $this);
+        $this->setError($this->name, $message);
+        return false;
+    }
+
+    /**
+     * Валидация данных.
+     * @param array|object маппинг значений [поле => значение]
+     * @param bool|null проверять на все ошибки
+     * @param bool|null вызывается ли из родительского набора полей
+     * @return bool
+     * @throws InvalidArgumentException
+     */
+    public function isValid($values, bool $multipleErrors = false, bool $fromParent = false): bool
+    {
+        if (!is_array($values) && !is_object($values)) {
+            if (true === $fromParent) {
+                return $this->buildError('valuesType');
+            } else {
+                throw new InvalidArgumentException(sprintf('Argument 1 $values '
+                   .' must be an array or an object, %s given', gettype($values)));
+            }
+        }
+
+        if (is_object($values)) $values = (array) $values;
+        $this->valuesBefore = &$values;
+        $this->values = [];
+        $this->errors()->clear();
+
+        foreach ($this->fields as $name => &$field) {
+            // наследуем экранирование html
+            $this->inheritHtmlEscaping($field, $name);
+
+            $isset = isset($values[$name]);
+            $value = $values[$name] ?? null;
+            if ($field instanceof static) {
+                // валидируем вложенный набор полей
+                if (empty($value)) $value = [];
+                try {
+                    if (false === $field->isValid($value, $multipleErrors, true)) {
+                        $this->setError($field->errors()->map());
+                        if (false === $multipleErrors) return false;
+                    }
+                } catch (InvalidArgumentException $e) {
+                    $this->setError($name, $e->getMessage());
+                    if (false === $multipleErrors) return false;
+                }
+                $this->values[$name] = $field->values;
+            }
+            else if ($field instanceof Field) {
+                // валидируем поле
+                if (false === $field->isValid($value, $isset)) {
+                    $this->setError($name, $field->error);
+                    if (false === $multipleErrors) return false;
+                }
+                if (empty($field->error) && !empty($field->same)) {
+                    $sameValue = $values[$field->same] ?? null;
+                    if ($sameValue !== $value) {
+                        $field->buildError('same');
+                        $this->setError($name, $field->error);
+                        if (false === $multipleErrors) return false;
+                    }
+                }
+                $this->values[$name] = $field->value;
+            }
+        }
+        if (!empty($this->errors()->has())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Проверка данных на валидность с выбрасом исключения в случае ошибки.
+     * @param array|object маппинг значений [поле => значение]
+     * @throws InvalidArgumentException
+     * @throws ValidateException
+     */
+    public function throwIfNotValid($values)
+    {
+        // if (!is_array($values) && !is_object($values)) {
+        //     throw new InvalidArgumentException(sprintf('Argument 1 $values must be an array or an object, %s given', gettype($key)));
+        // }
+        if (false === $this->isValid($values)) {
+            throw new ValidateException($this->errors()->first());
+        }
+    }
+}
