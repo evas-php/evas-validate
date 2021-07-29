e4cce83c7e26dc80edb48a947207e548712b4d06
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-29 15:40:25 +0300

    Create error_codes.php

---
+++ src/config/error_codes.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Коды ошибок валидации.
+ */
+return [
+    // неизвестная ошибка
+    'unknown' => 0,
+    // ошибки валидации поля
+    'required' => 1,
+    'undefinedType' => 2,
+    'type' => 3,
+    'length' => 4,
+    'range' => 5,
+    'pattern' => 6,
+    'same' => 7,
+    'options' => 8,
+    'optionsSetting' => 9,
+    // ошибки валидации набора полей
+    'valuesType' => 10,
+    'jsonParse' => 11,
+    'jsonEmpty' => 12,
+    'jsonType' => 13,
+];
