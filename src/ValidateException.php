198e911897d562fc933bfa88911fd274384ed319
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-29 15:36:29 +0300

    Create ValidateException.php

---
+++ src/ValidateException.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Исключение валидации.
+ * @package evas-php\evas-validate
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Validate;
+
+class ValidateException extends \Exception
+{}
