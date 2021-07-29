bba7024519ef807a645439549784ce9ebd0fef87
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-29 15:40:48 +0300

    Create errors_default.php

---
+++ src/config/errors_default.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Ошибки валидации по умолчанию.
+ */
+return [
+    // неизвестная ошибка
+    0 => 'error.validate.unknown',
+    // ошибки валидации поля
+    1 => 'error.validate.field.required',
+    2 => 'error.validate.field.undefinedType',
+    3 => 'error.validate.field.type',
+    4 => 'error.validate.field.length',
+    5 => 'error.validate.field.range',
+    6 => 'error.validate.field.pattern',
+    7 => 'error.validate.field.equals',
+    8 => 'error.validate.field.options',
+    9 => 'error.validate.field.optionsSetting',
+    // ошибки валидации набора полей
+    10 => 'error.validate.fieldset.valuesType',
+    11 => 'error.validate.fieldset.jsonParse',
+    12 => 'error.validate.fieldset.jsonEmpty',
+    13 => 'error.validate.fieldset.jsonType',
+];
