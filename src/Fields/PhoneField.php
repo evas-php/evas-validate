d91ba43294695478ea3ce66847bf1c0a7560ef1f
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-08-08 05:58:58 +0300

    Create PhoneField.php

---
+++ src/Fields/PhoneField.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Валидатор телефона.
+ * @package evas-php/evas-validate
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Validate\Fields;
+
+use Evas\Validate\Field;
+
+class PhoneField extends Field
+{
+    public $min = 10;
+    public $max = 13;
+    public $pattern = '/(\d*)(\d{10})$/';
+
+    public function prepareValue()
+    {
+        $pattern = '/\d/';
+        preg_match_all($pattern, $this->value, $matches);
+        $this->value = implode('', $matches[0]);
+    }
+}
